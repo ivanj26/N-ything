528af7dc1e6fb93a80860eb07ceaeddb28b815c7
Muhammad Alfian Rasyidin <13516104@std.stei.itb.ac.id>
2018-09-18 22:49:29 +0700

    add Board class

---
+++ Board.py
@@ -0,0 +1,16 @@
+class Board:
+	self._MAX_ROW = 8
+	self._MAX_COLUMN = 8
+	self._list_of_pieces = []
+
+	def __init__(self):
+
+	def is_out_of_bound(self):
+
+	def is_move_valid(self):
+
+	def random_pick(self):
+
+	def random_move(self):
+
+	def calculate_heuristic(self):
